4f9a2566aff28edc72e68066d62d4412286a6c8a
adalarcon <31774967+adalarcon@users.noreply.github.com>
2018-09-19 15:19:28 -0600

    set up

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const CategoriesCTRL = require('../controller/categories.controller');
+const CustomersCTRL = require('../controller/customers.controller');
 
 router.get("/", (req, res) =>{
   res.send("Welcome to data api");
@@ -13,5 +14,12 @@ router.get('/api/v1/categories/:id', CategoriesCTRL.findById);
 router.post('/api/v1/categories', CategoriesCTRL.insertOne);
 router.put('/api/v1/categories',  CategoriesCTRL.updateOne);
 router.delete('/api/v1/categories/:id',  CategoriesCTRL.deleteOne);
+
+
+router.get('/api/v1/customers',  CategoriesCTRL.find);
+router.get('/api/v1/customers/:id', CategoriesCTRL.findById);
+router.post('/api/v1/customers', CategoriesCTRL.insertOne);
+router.put('/api/v1/customers',  CategoriesCTRL.updateOne);
+router.delete('/api/v1/customers/:id',  CategoriesCTRL.deleteOne);
 
 module.exports = router;
