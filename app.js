e0eb8a896c2178af7d57d7e2e35636180692ab51
adalarcon <31774967+adalarcon@users.noreply.github.com>
2018-09-22 12:59:50 -0600

    heroku ping

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const app   = require('./config/express');
 var server = app.listen(app.get('port'), function() {
   console.log('[app] Express server listening on port ' + server.address().port);
 });
+
+setInterval(function() {
+    http.get("https://dataservicemx.herokuapp.com/api/v1/");
+}, 300000);
